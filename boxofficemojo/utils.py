467cb058bbc764d728ab8380d72b625a6d8f9517
ryansharp <rtsharp90@gmail.com>
2014-10-06 16:20:38 -0700

    began writing html parsing code

--- boxofficemojo/utils.py
+++ boxofficemojo/utils.py
@@ -1,3 +1,5 @@
+from bs4 import BeautifulSoup
+
 from lxml import html
 from io import StringIO
 from datetime import date, timedelta
@@ -12,6 +14,19 @@ import urllib
 def pullMovieDataFromList(list_id):
 	request_url = "http://boxofficemojo.com/movies/alphabetical.htm?letter={0}&p=.htm".format(list_id)
 	try:
-		pass
+		html_doc = urllib2.urlopen(request_url).read()
+		soup = BeautifulSoup(html_doc)
+		html_tables = soup.find_all('table')
+		movie_list = html_tables[3].find_all('tr')
+		count = 0
+		for movie in movie_list:
+			if count = 0:
+				count += 1
+				continue
+			movie_datas = movie.find_all('td')
+			movie_name = movie_datas[0].find_all('b')[0].text
+			movie_data_url = movie_datas[0].find_all('a')[0].get('href')
+			movie_studio = movie_datas[1].find_all('font')[0].text
+			
 	except Exception, e:
 		print e
