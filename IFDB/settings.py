ee57ae378abd93c15067b524c71a2eac0723a326
ryansharp <rtsharp90@gmail.com>
2014-10-06 15:38:10 -0700

    added app for boxofficemojo.com scraper

--- IFDB/settings.py
+++ IFDB/settings.py
@@ -124,6 +124,7 @@ INSTALLED_APPS = (
     'profiles',
     'movies',
     'social',
+    'boxofficemojo',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
