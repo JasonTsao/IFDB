2f361e8ace5eeec61e5a757a1b085a89cd83fc9f
ryansharp <rtsharp90@gmail.com>
2014-10-06 17:01:52 -0700

    writing code for pulling page specific movie data

--- boxofficemojo/utils.py
+++ boxofficemojo/utils.py
@@ -11,6 +11,11 @@ import json
 import urllib2
 import urllib
 
+
+def syncBoxOfficeMojoData():
+	pass
+
+
 def pullMovieDataFromList(list_id):
 	request_url = "http://boxofficemojo.com/movies/alphabetical.htm?letter={0}&p=.htm".format(list_id)
 	try:
@@ -27,6 +32,30 @@ def pullMovieDataFromList(list_id):
 			movie_name = movie_datas[0].find_all('b')[0].text
 			movie_data_url = movie_datas[0].find_all('a')[0].get('href')
 			movie_studio = movie_datas[1].find_all('font')[0].text
-			
+			total_gross = int(movie_datas[2].find_all('font')[0].text[1:])
+			total_theaters = int(movie_datas[3].fidn_all('font')[0].text)
+			opening_gross = int(movie_datas[4].find_all('font')[0].text[1:])
+			opening_theaters = int(movie_datas[5].find_all('font')[0].text[1:])
+			opening = movie_datas[6].find_all('a')[0].text
+			details = pullMoviePageData(movie_data_url)
+	except Exception, e:
+		print e
+
+
+def pullMoviePageData(data_url):
+	request_url = "http://boxofficemojo.com{}".format(data_url)
+	try:
+		data = {}
+		html_doc = urllib2.urlopen(request_url).read()
+		soup = BeautifulSoup(html_doc)
+		data_divs = soup.find_all(attrs={"class": "mp_box"})
+		for data_div in data_divs:
+			data_table = data_div.find_all('table')[0]
+			data_rows = data_table.find_all('tr')
+			for row in data_rows:
+				results = None
+				row_datas = row.find_all('td')
+				list_results = row_datas[1].find_all('a')
+				data[row_datas[0].text[:-1]] = row_datas[1].text
 	except Exception, e:
 		print e
