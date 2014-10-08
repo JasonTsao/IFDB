d6c6a8f9f0663b130643a1dd0aa603961a579efd
JasonTsao <lanevo6tmegsr@gmail.com>
2014-10-08 14:42:37 -0700

    adding new userprofile model and control code

--- profiles/admin.py
+++ profiles/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.contrib.admin import site, ModelAdmin
 from models import *
 
+admin.site.register(UserProfile)
 admin.site.register(Profile)
 admin.site.register(CollaborationLink)
